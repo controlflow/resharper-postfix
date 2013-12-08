29b91adce8993d4f429c983a0019fa86524eee92
ControlFlow <shvedov89@gmail.com>
2013-12-08 04:16:43 +0400

    rr

---
+++ TemplateProviders/CheckIsNullTemplate.cs
@@ -0,0 +1,35 @@
+﻿using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.ReSharper.Psi.ControlFlow.CSharp;
+
+namespace JetBrains.ReSharper.ControlFlow.PostfixCompletion.TemplateProviders
+{
+  [PostfixTemplate(
+    templateName: "null",
+    description: "Checks expression to be null",
+    example: "if (expr == null)")]
+  public class CheckIsNullTemplate : CheckForNullTemplateBase, IPostfixTemplate {
+    public ILookupItem CreateItems(PostfixTemplateContext context) {
+      var exprContext = context.OuterExpression;
+      if (!exprContext.CanBeStatement) return null;
+
+      if (!context.ForceMode) {
+        if (exprContext.Type.IsUnknown) return null;
+        if (!IsNullableType(exprContext.Type)) return null;
+      }
+
+      var state = CSharpControlFlowNullReferenceState.UNKNOWN;
+      if (!context.ForceMode) {
+        state = CheckNullabilityState(exprContext);
+      }
+
+      switch (state) {
+        case CSharpControlFlowNullReferenceState.MAY_BE_NULL:
+        case CSharpControlFlowNullReferenceState.UNKNOWN: {
+          return new CheckForNullItem("null", exprContext, "if(expr==null)");
+        }
+      }
+
+      return null;
+    }
+  }
+}
