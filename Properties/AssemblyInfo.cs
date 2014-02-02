05e10d7ca52c17a86a0d8b465b6bde4b8b75e0c4
ControlFlow <shvedov89@gmail.com>
2014-02-03 01:00:59 +0400

    1.9.1-beta

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyVersion("1.9.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: PluginTitle("Postfix Templates")]
