b05c6e230168875c2d3c32c3d1b768f398a23028
ControlFlow <shvedov89@gmail.com>
2014-05-14 04:15:21 +0400

    2.0.4

--- PostfixTemplates/Templates/IntroduceVariableTemplate.cs
+++ PostfixTemplates/Templates/IntroduceVariableTemplate.cs
@@ -27,6 +27,8 @@ using JetBrains.ReSharper.Refactorings.WorkflowNew;
 using JetBrains.ReSharper.Feature.Services.Refactorings;
 #endif
 
+// todo: think about cases like F(this.var), F(42.var). disable in auto?
+
 namespace JetBrains.ReSharper.PostfixTemplates.Templates
 {
   [PostfixTemplate(
