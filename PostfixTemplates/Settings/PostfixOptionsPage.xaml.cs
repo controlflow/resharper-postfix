2c5ea1085aac306142f30a51bc59c9d6901f1d7f
ControlFlow <shvedov89@gmail.com>
2015-04-14 23:23:47 +0300

    Various

--- PostfixTemplates/Settings/PostfixOptionsPage.xaml.cs
+++ PostfixTemplates/Settings/PostfixOptionsPage.xaml.cs
@@ -3,24 +3,23 @@ using System.Windows.Input;
 using JetBrains.Annotations;
 using JetBrains.DataFlow;
 using JetBrains.ReSharper.Feature.Services.Resources;
-using JetBrains.ReSharper.Features.Intellisense.Options;
 using JetBrains.UI.CrossFramework;
 using JetBrains.UI.Options;
+using JetBrains.UI.Options.OptionPages;
 
 namespace JetBrains.ReSharper.PostfixTemplates.Settings
 {
   [OptionsPage(
     id: PID, name: "Postfix Templates",
     typeofIcon: typeof(ServicesThemedIcons.SurroundTemplate),
-    ParentId = IntelliSensePage.PID)]
+    ParentId = EnvironmentPage.Pid)]
   public sealed partial class PostfixOptionsPage : IOptionsPage
   {
     // ReSharper disable once InconsistentNaming
     private const string PID = "PostfixTemplates";
 
-    public PostfixOptionsPage([NotNull] Lifetime lifetime,
-      [NotNull] OptionsSettingsSmartContext store,
-      [NotNull] PostfixTemplatesManager templatesManager)
+    public PostfixOptionsPage([NotNull] Lifetime lifetime, [NotNull] OptionsSettingsSmartContext store,
+                              [NotNull] PostfixTemplatesManager templatesManager)
     {
       InitializeComponent();
       DataContext = new PostfixOptionsViewModel(lifetime, store, templatesManager);
